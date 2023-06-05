0fe8cfcee2435fe75c6cb9a5399df32555844fa5
sammpudi <111079306+sammpudi@users.noreply.github.com>
2023-06-05 18:35:16 +0200

    about.php
    The about pge

---
+++ about.php
@@ -0,0 +1,315 @@
+
+<?php include 'footer.php' ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style/style.css">
+  <script src="https://kit.fontawesome.com/b2e0a616b3.js" crossorigin="anonymous"></script>
+  <title>ThriveMentor</title>
+  <link rel="icon" href="images/Creative-PhotoRoom.png-PhotoRoom.png" type="image/png">
+</head>
+
+<body>
+  <div class="nav">
+    <img src="images/Creative-PhotoRoom.png-PhotoRoom.png" style="width: 120px;">
+    <div class="logo">
+
+
+    </div>
+    <div class="right-links">
+      <ul>
+        <li><a href="Home.php">HOME</a></li>
+        <li><a href="Services.php">SERVICES</a></li>
+        <li><a href="Coaches.php">COACHES</a></li>
+        <li><a href="profile.php">PROFILE</a></li>
+
+        <a href="contact.php"><button class="btn">Contact Us</button></a>
+        <a href="./configuration/logout.php"><button class="btn">Log Out</button></a>
+      </ul>
+    </div>
+  </div>
+  <main>
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="text-box">
+      <h1 id="heione" style="font-size: 65px;">About Us</h1>
+      <p>Our company strives in the health of every individual and is about taking care people who do not have the
+        strenght or courage to seek for help whilst. As a company this is the platform for them too voice out
+        their problems so they can be assisted without them having to go to physical location</p>
+
+
+      <p>
+      <div class="about-message">
+
+
+        <p>We're thrilled to have you here. At ThriveMentor, our mission is to create a safe and supportive space for your personal growth and empowerment.
+          We understand that life can present challenges, and that's why we're here to guide you on your journey towards thriving.
+
+          Whether you're seeking guidance, inspiration, or mentorship, you've come to the right place. Our team of experienced mentors is dedicated to helping you unlock your full potential, overcome obstacles, and achieve your goals.
+          We believe that everyone deserves the opportunity to flourish and lead a fulfilling life.
+
+          In our welcoming community, you'll find resources, tools, and a network of like-minded individuals who are passionate about personal development.
+          Feel free to explore our articles, join our interactive workshops, or connect with mentors who can provide the guidance you need. We're here to support you every step of the way.
+
+          Your growth is important to us, and we're committed to providing you with a nurturing environment where you can thrive. We encourage you to embrace this journey, embrace your potential, and embrace the possibilities that lie ahead.
+
+          Thank you for choosing ThriveMentor. Together, let's embark on a transformative path towards personal growth and empowerment.
+
+          Warmest wishes,</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+      </p>
+
+
+
+
+
+    </div>
+
+    <section class="mission">
+      <div class="mission-content">
+        <h1>Our Mission</h1>
+        <p>At Thrive Mentor, our mission is to provide accessible and compassionate mental health support to individuals in need. We aim to empower individuals to overcome challenges, develop resilience, and lead fulfilling lives. Through our platform, we strive to create a safe and supportive community where people can find guidance, resources, and personalized solutions for their mental well-being.</p>
+      </div>
+      <div class="support">
+        <img src="images/yoga.png" alt="Meditation">
+      </div>
+    </section>
+
+
+
+    <section class="vision">
+      <div class="priority">
+        <img src="images/support.png" alt="Support">
+      </div>
+      <div class="vision-content">
+        <h1>Our Vision</h1>
+        <p>Our vision is a world where mental health is prioritized, understood, and destigmatized. We envision a society where everyone has access to high-quality mental health support and resources, regardless of their background or circumstances. Through continuous innovation and collaboration, we seek to revolutionize the way mental health is approached, ensuring that individuals receive the care they deserve and can thrive in all aspects of their lives.</p>
+      </div>
+    </section>
+
+
+    <section class="partnerships">
+  <h2>Our Patners</h2>
+  <div class="partners">
+    <div class="partner">
+      <img src="images/phila (1).png" alt="Partner 1" class="partner-logo" style="width: 200px;">
+    </div>
+    <div class="partner">
+      <img src="images/phila (2).png" alt="Partner 2" class="partner-logo">
+    </div>
+    <div class="partner">
+      <img src="images/live.png" alt="Partner 3" class="partner-logo">
+    </div>
+    
+  </div>
+</section>
+
+    <div class="faq-section">
+  <h2 class="section-title">Frequently Asked Questions</h2>
+  <div class="faq">
+    <h3 class="faq-question">How does ThriveMentor work?</h3>
+    <p class="faq-answer">ThriveMentor is an online platform that provides a safe and supportive space for personal growth and empowerment. We connect individuals with experienced mentors who can guide them on their journey towards overcoming challenges, achieving their goals, and unlocking their full potential.</p>
+  </div>
+  <div class="faq">
+    <h3 class="faq-question">Are the mentors qualified and experienced?</h3>
+    <p class="faq-answer">Yes, most of our mentors are highly qualified and experienced in their respective fields. We carefully vet and select mentors who have the expertise and passion for helping others thrive. They are committed to providing personalized guidance and support to individuals seeking assistance.</p>
+  </div>
+  <div class="faq">
+    <h3 class="faq-question">What types of challenges can I seek assistance for?</h3>
+    <p class="faq-answer">Our mentors can provide guidance and support for a wide range of challenges, including personal development, career growth, relationship issues, stress management, and goal setting. Whatever challenge you're facing, our mentors are here to help you navigate through it.</p>
+  </div>
+  <div class="faq">
+    <h3 class="faq-question">How do I get started with ThriveMentor?</h3>
+    <p class="faq-answer">Getting started is easy! Simply sign up on our website. From there, you can schedule your first session and begin your journey towards personal growth and empowerment.</p>
+  </div>
+</div>
+
+
+
+
+
+
+    </div>
+
+
+
+
+    <form class="search">
+      <input type="text" name="search" id="search" placeholder="Search" class="search--input">
+      <button type="button" class="serach--btn">
+        <img src="images/search-bar.png" class="svg">
+      </button>
+    </form>
+
+
+
+
+
+
+
+
+
+  </main>
+  <style>
+    .right-links {
+      background: #0f2d49;
+      border-radius: 1.2em;
+      height: 65px;
+    }
+
+    .mission {
+      display: flex;
+      align-items: center;
+      margin-top: 200px;
+      margin-left: 30px;
+    }
+
+    .mission-content {
+      flex: 1;
+    }
+
+    .mission-content p {
+      color: white;
+      font-size: 15px;
+    }
+
+    .support {
+      margin-left: 40px;
+      margin-right: 20px;
+    }
+
+
+    .vision {
+      display: flex;
+      align-items: center;
+      margin-top: 100px;
+    }
+
+    .priority {
+      margin-right: 20px;
+      text-align: right;
+      margin-left: 20px;
+      border-radius: 1.2em;
+    }
+
+    .vision-content {
+      flex: 1;
+      text-align: left;
+      margin-left: 20px;
+      color: aliceblue;
+
+    }
+    .faq-section {
+  margin-top: 40px;
+}
+
+.section-title {
+  font-size: 28px;
+  font-weight: bold;
+  text-align: center;
+  color:rgb(216, 202, 3);
+}
+
+.faq {
+  margin-bottom: 30px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width:1300px;
+  margin-left: 50px;
+}
+
+.faq-question {
+  font-size: 22px;
+  font-weight: bold;
+  color: #2979ff;
+}
+
+.faq-answer {
+  font-size: 18px;
+  line-height: 1.5;
+  color: #555555;
+}
+
+.partnerships {
+  text-align: center;
+  margin-bottom: 60px; 
+  position: relative;
+  z-index: 5;
+}
+
+.partnerships h2 {
+  font-size: 28px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.partners {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.partner {
+  margin: 10px;
+  animation: partner-animation 1.5s ease-in-out infinite alternate;
+}
+
+.partner-logo {
+  width: 200px;
+  height: auto;
+  max-height: 150px; 
+  object-fit: contain;
+}
+
+@keyframes partner-animation {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.2);
+  }
+}
+
+
+
+
+    
+  </style>
+
+
+
+</body>
+
+</html>
