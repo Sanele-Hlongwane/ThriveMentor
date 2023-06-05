e178949443f71726f790f7dac15f9b8d79858769
sammpudi <111079306+sammpudi@users.noreply.github.com>
2023-06-05 18:45:47 +0200

    thivebot.php

---
+++ thivebot.php
@@ -0,0 +1,63 @@
+
+
+
+
+<?php include 'footer.php' ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ThriveBot2</title>
+    <link rel="icon" href="images/Creative-PhotoRoom.png-PhotoRoom.png" type="image/png">
+</head>
+<body>
+                   
+
+  <h1>Try our ChatBot</h1>
+
+<iframe 
+  src="https://instantanswers.xyz/chatbots/7c1fc050-0bc4-4f23-bdb3-8e1d6193a483/iframe" 
+  width="500px" 
+  height="400px" 
+  frameborder="0"   class="second-bot">
+</iframe>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+    <style>
+      .second-bot{
+        position: absolute;
+        left: 30%;
+        top: 20%;
+        margin-bottom:300px;
+      }
+      body{
+        background:#0F2D49;
+      }
+      h1{
+         position: absolute;
+         left: 30%;
+         color: rgb(216, 200, 3);
+      }
+      iframe{
+        margin-bottom: 200px;
+      }
+    </style>
+
+
+</html>
