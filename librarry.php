1bcdadfa8b6acd6b1e9a60ea89648a7e658fc638
sammpudi <111079306+sammpudi@users.noreply.github.com>
2023-06-05 18:56:04 +0200

    Create librarry.php

---
+++ librarry.php
@@ -0,0 +1,247 @@
+
+<?php include 'footer.php'; ?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our ChatBots</title>
+    <link rel="icon" href="images/Creative-PhotoRoom.png-PhotoRoom.png" type="image/png">
+</head>
+<body>
+<div class="container">
+    <h1>Mental Health Library Resources</h1>
+    
+    <div class="resource-card">
+      <h2 class="resource-title">Crisis Helpline</h2>
+      <p class="resource-description">If you're in crisis and need immediate assistance, call the Crisis Helpline for confidential support.</p>
+      <a class="resource-link" href="https://www.sadag.org/index.php?option=com_content&view=article&id=11&Itemid=114">SADAG Helpline</a>
+    </div>
+    
+    
+    
+    <div class="resource-card">
+      <h2 class="resource-title">Support Groups</h2>
+      <p class="resource-description">Join support groups where you can connect with others who are going through similar challenges.</p>
+      <a class="resource-link" href="https://www.sadag.org/index.php?option=com_content&view=article&id=3215:support-groups-mental-health&catid=96:support-groups&Itemid=245">Find a Group</a>
+    </div>
+
+   
+    <div class="resource-card">
+      <h2 class="resource-title">Online mental health and wellness coaching</h2>
+      <p class="resource-description">Develop your mental reselince by watching our online wellness session</p>
+      <a class="resource-link" href="onlinevid.php">Visit site</a>
+    </div>
+
+
+    <div class="resource-card">
+      <h2 class="resource-title">Informative  articles </h2>
+      <p class="resource-description">Read our informative mental health and wellness articles  to assist you in managing stress,anxiety and the importance of self-care</p>
+      <a class="resource-link" href="articles.php">Visit site</a>
+    </div>
+
+
+    <div class="resource-card">
+      <h2 class="resource-title">Blogs </h2>
+      <p class="resource-description">Read our informative blogs where users from all walks of life  share their experiences on how they have preveously dealt with mental health issues and gain inights from them</p>
+      <a class="resource-link" href="news.php">Visit site</a>
+    </div>
+
+
+
+
+
+
+
+  </div>
+</body>
+
+<style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background:#0F2D49;
+    }
+    
+    .container {
+      max-width: 800px;
+      margin: 0 auto;
+      padding: 20px;
+      color:white;
+    }
+    
+    h1 {
+      color:#b9b901;
+    }
+    
+    .resource-card {
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      padding: 20px;
+      margin-bottom: 20px;
+      background: white;
+    }
+    
+    .resource-title {
+      font-size: 18px;
+      font-weight: bold;
+      margin-bottom: 10px;
+      color: black;
+    }
+    
+    .resource-description {
+      color: #666;
+      margin-bottom: 10px;
+    }
+    
+    .resource-link {
+      color: #0066cc;
+      text-decoration: none;
+    }
+    
+    .resource-link:hover {
+      text-decoration: underline;
+    }
+    .container{
+      margin-bottom: 300px;
+      
+    }
+  </style>
+
+
+    
+</body>
+</html>
+<?php include 'footer.php'; ?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our ChatBots</title>
+    <link rel="icon" href="images/Creative-PhotoRoom.png-PhotoRoom.png" type="image/png">
+</head>
+<body>
+<div class="container">
+    <h1>Mental Health Library Resources</h1>
+    
+    <div class="resource-card">
+      <h2 class="resource-title">Crisis Helpline</h2>
+      <p class="resource-description">If you're in crisis and need immediate assistance, call the Crisis Helpline for confidential support.</p>
+      <a class="resource-link" href="https://www.sadag.org/index.php?option=com_content&view=article&id=11&Itemid=114">SADAG Helpline</a>
+    </div>
+    
+    
+    
+    <div class="resource-card">
+      <h2 class="resource-title">Support Groups</h2>
+      <p class="resource-description">Join support groups where you can connect with others who are going through similar challenges.</p>
+      <a class="resource-link" href="https://www.sadag.org/index.php?option=com_content&view=article&id=3215:support-groups-mental-health&catid=96:support-groups&Itemid=245">Find a Group</a>
+    </div>
+
+   
+    <div class="resource-card">
+      <h2 class="resource-title">Online mental health and wellness coaching</h2>
+      <p class="resource-description">Develop your mental reselince by watching our online wellness session</p>
+      <a class="resource-link" href="onlinevid.php">Visit site</a>
+    </div>
+
+
+    <div class="resource-card">
+      <h2 class="resource-title">Informative  articles </h2>
+      <p class="resource-description">Read our informative mental health and wellness articles  to assist you in managing stress,anxiety and the importance of self-care</p>
+      <a class="resource-link" href="articles.php">Visit site</a>
+    </div>
+
+
+    <div class="resource-card">
+      <h2 class="resource-title">Blogs </h2>
+      <p class="resource-description">Read our informative blogs where users from all walks of life  share their experiences on how they have preveously dealt with mental health issues and gain inights from them</p>
+      <a class="resource-link" href="news.php">Visit site</a>
+    </div>
+
+
+
+
+
+
+
+  </div>
+</body>
+
+<style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background:#0F2D49;
+    }
+    
+    .container {
+      max-width: 800px;
+      margin: 0 auto;
+      padding: 20px;
+      color:white;
+    }
+    
+    h1 {
+      color:#b9b901;
+    }
+    
+    .resource-card {
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      padding: 20px;
+      margin-bottom: 20px;
+      background: white;
+    }
+    
+    .resource-title {
+      font-size: 18px;
+      font-weight: bold;
+      margin-bottom: 10px;
+      color: black;
+    }
+    
+    .resource-description {
+      color: #666;
+      margin-bottom: 10px;
+    }
+    
+    .resource-link {
+      color: #0066cc;
+      text-decoration: none;
+    }
+    
+    .resource-link:hover {
+      text-decoration: underline;
+    }
+    .container{
+      margin-bottom: 300px;
+      
+    }
+  </style>
+
+
+    
+</body>
+</html>
