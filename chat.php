8e3bc2aaff838184c76d889aa5ad2eacbef92580
sammpudi <111079306+sammpudi@users.noreply.github.com>
2023-06-05 18:44:17 +0200

    chat.php
    file for the chatbot

---
+++ chat.php
@@ -0,0 +1,489 @@
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<title>ThriveMentor</title>
+    <link rel="icon" href="images/Creative-PhotoRoom.png-PhotoRoom.png" type="image/png">
+        
+		<script 
+            src="https://instantanswers.xyz/instant-answers-chat.min.js" 
+         chatbot-id="7c1fc050-0bc4-4f23-bdb3-8e1d6193a483"> 
+        </script>
+
+
+
+
+
+
+
+
+
+
+
+
+	</head>
+	<body>
+	<div class="box">
+    <h1>ThriveBot1</h1>
+
+    <div class="imagee">
+      <img src="images/chat1.jpg" alt="Chatbot Image">
+    </div>
+
+    
+    <div class="box">
+			
+			<div class="chat-container">
+				<div class="chat-box" id="chat-box"></div>
+				<form id="chat-form">
+					<input type="text" placeholder="Type to start a chat.." class="input" id="chat-input" />
+				</form>
+			</div>
+		</div>
+
+	
+
+
+
+
+
+
+    <div class="capabilities-section">
+      <h2 class="capabilities-title">Capabilities</h2>
+      <ul class="capabilities-list">
+        <li>Provides recommendations and advice</li>
+        <li>Offers support for mental health topics and challanges</li>
+        <li>Responds to user queries and provides relevant information</li>
+        <li>Guides users through interactive conversations</li>
+        
+      </ul>
+
+      <h2 class="limitations-title">Limitations</h2>
+      <ul class="limitations-list">
+        <li>Does not have access to real-time data </li>
+        <li>Responses are based on pre-programmed knowledge and patterns</li>
+        <li>May not understand complex or ambiguous queries</li>
+       
+      </ul>
+    </div>
+  </div>
+    
+
+	</body>
+
+
+
+
+
+    <footer class="footer-distributed">
+
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="footer-left">
+            <h3>Thrive <span>Mentor.io</span></h3>
+            <p class="footer-icons">
+
+
+<a href="https://www.linkedin.com/company/thrivementor/?viewAsMember=true"><i class="fa fa-linkedin"></i></a>    
+<a href="https://twitter.com/ThriveMentor" class="fa fa-twitter"></a>
+
+<a href="https://www.facebook.com/profile.php?id=100093026431060"><i class="fa fa-facebook"></i></a>
+
+
+
+             
+             
+  
+     </p>
+            <p class="footer-links">
+                <a href="Home.php">Home</a>
+                |
+                <a href="About.php">About</a>
+                |
+                <a href="Services.php">Services</a>
+                |
+                <a href="Coaches.php">Coaches</a>
+                |
+                <a href="profile.php">Profile</a>
+                
+            </p>
+           
+           
+        </div>
+        <div class="footer-center">
+            <div>
+                <i class="fa-solid fa-location-dot"></i>
+                <p><span>UJ Bunting Road</span>Auckland  Park</p>
+            </div>
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>011 559 4555</p>
+            </div>
+            <div>
+                
+			<i class="fa fa-envelope" style="font-size:36px"></i>
+
+                <p> mentorthrive79@gmail.com</p>
+            </div>
+           
+
+        </div>
+        <div class="footer-right">
+            <p class="footer-company-about">
+                <span>About the company</span>
+                <b>ThriveMentor.io</b> is a website that is built for people in need of help and willing to be assisted dealing with their issues <br>
+                &copy; 2023 ThriveMentor. All rights reserved.  <a href="terms-and-conditions.html">Terms of Service</a>
+            </p>
+        </div>
+
+        
+        
+
+        
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<script src="app.js"></script>
+	<style>
+		@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap");
+		body {
+      background-color: #f2f2f2;
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+	  background: #0F2D49;
+	  max-height: 1200px;
+    }
+
+    .box {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+	  margin-bottom: 600px;
+	  margin-top: 295px;
+    }
+
+    h1 {
+      font-size: 28px;
+      color:rgb(216, 200, 3);
+      margin-bottom: 20px;
+      text-align: center;
+    }
+
+    .imagee {
+      margin-bottom: 20px;
+      animation: floatAnimation 3s ease-in-out infinite;
+    }
+
+    .imagee img {
+      width: 200px;
+      height: 200px;
+      border-radius: 50%;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    @keyframes floatAnimation {
+      0% {
+        transform: translateY(0);
+      }
+      50% {
+        transform: translateY(-10px);
+      }
+      100% {
+        transform: translateY(0);
+      }
+    }
+
+
+
+
+
+	.capabilities-section {
+      margin-top: 20px;
+    }
+
+    .capabilities-title {
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+
+    .capabilities-list {
+      margin-bottom: 20px;
+    }
+
+    .limitations-title {
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+
+    .limitations-list {
+      margin-bottom: 20px;
+    }
+	h2{
+		color: rgb(216, 200, 3);
+	}
+	ul li{
+		color: aliceblue;
+	}
+    footer{
+    position:fixed;
+  bottom: 0%;
+    width: 100%;
+    height: 25%;
+}
+.footer-distributed{
+    background-color: #292c2f;
+    box-sizing: border-box;
+    widows: 100%;
+    text-align: left;
+    font: bold 10px sans-serif;
+    padding: 5px 5px 5px 5px;
+    margin-top: 50px;
+}
+.footer-distributed .footer-left, .footer-distributed .footer-center, .footer-distributed .footer-right{
+    display: inline-block;
+    vertical-align: top;
+}
+.footer-distributed .footer-left{
+    width: 30%;
+}
+.footer-distributed h3{
+    color: #ffff;
+    font: normal 36px 'cookie',cursive;
+    margin: 0;
+}
+.footer-distributed h3 span{
+    color: #ccbf90;
+    font: normal 36px 'cookie',cursive;
+}
+.footer-distributed .footer-links{
+    color: #ffff;
+    margin: 20px 0 12px;
+}
+.footer-distributed .footer-links a{
+    display: inline-block;
+    line-height: 1.8;
+    text-decoration: none;
+    color: inherit;
+}
+.footer-distributed .footer-company-name{
+    color: #8f9296;
+    font-size: 14px;
+    font-weight: normal;
+    margin: 0;
+}
+.footer-distributed .footer-center{
+    width: 35%;
+}
+.footer-distributed .footer-center i{
+    background-color: #33383b;
+    color: #ffff;
+    font-size: 25px;
+    width: 38px;
+    height: 38px;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 42px;
+    margin: 10px 15px;
+    vertical-align: middle;
+}
+.footer-distributed .footer-center i .fa-envelop{
+    font-size: 10px;
+    line-height: 38px;
+}
+.footer-distributed .footer-center p{
+    display: inline-block;
+    color: #ffff;
+    vertical-align: middle;
+    margin: 0;
+}
+.footer-distributed .footer-center p span{
+    display:block;
+    font-weight: normal;
+    font-size:14px;
+    line-height:2;
+}
+.footer-distributed .footer-center p a{
+    color:  #5383d3;
+    text-decoration: none;
+}
+.footer-distributed .footer-right{
+    width: 30%;
+}
+.footer-distributed .footer-company-about{
+    line-height: 20px;
+    color: #ffff;
+    font-size: 13px;
+    font-weight: normal;
+    margin: 0;
+}
+.footer-distributed .footer-company-about span{
+    display: block;
+    color:  #5383d3;
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 20px;
+}
+.footer-distributed .footer-icons{
+    margin-top: 25px;
+    display: block;
+}
+.footer-distributed .footer-icons a{
+    display: inline-block;
+    width: 35px;
+    height: 35px;
+    cursor: pointer;
+    background-color:  #33383b;
+    border-radius: 2px;
+    font-size: 20px;
+    color: #ffffff;
+    text-align: center;
+    line-height: 35px;
+    margin-right: 3px;
+    margin-bottom: 5px;
+}
+.footer-distributed .footer-icons a:hover{
+    background-color: #3f71ea;
+}
+.footer-links a:hover{
+    color: #3f71ea;
+} 
+
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap");
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+
+body {
+	display: grid;
+	place-items: center;
+	height: 100vh;
+	font-family: "Poppins", "sans-serif";
+}
+
+h1 {
+	margin-bottom: 1rem;
+	text-align: center;
+}
+
+span {
+	font-size: 0.9rem;
+}
+
+.box {
+	width: 350px;
+}
+
+.bot,
+.user {
+	padding: 0.5rem 0.5rem;
+	width: fit-content;
+	border-radius: 0.5rem;
+	background-color: #e0f033ca;
+	max-width: 200px;
+  color: white;
+}
+
+.user {
+	background-color: #8f0aa0;
+	color: white;
+	align-self: end;
+}
+
+.chat-container {
+	border: 2px solid black;
+	border-radius: 0.5rem;
+	padding: 1rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	gap: 1rem;
+}
+
+.chat-box {
+	height: 300px;
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	padding: 0.5rem 0.75rem 0.5rem 0;
+	overflow-y: auto;
+  width: 420px;
+}
+
+.input {
+	width: 100%;
+	padding: 0.5rem;
+	accent-color: #42855b;
+	font-family: "Poppins", "sans-serif";
+}
+
+
+
+
+
+
+
+
+
+	</style>
+</html>
