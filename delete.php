12d290763cc83b0bc8f446ddb544d24d119f9fee
sammpudi <111079306+sammpudi@users.noreply.github.com>
2023-06-05 18:59:52 +0200

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    include("./configuration/config.php");
+
+    if(isset($_GET['Id'])){
+        $id = $_GET['Id'];
+        $sql = "DELETE FROM user WHERE Id= $id";
+        $result = mysqli_query($connection, $sql);
+
+        echo "<div class = 'message'>
+                   <p> User Deleted </p>
+                </div> </br>";
+
+    }
+    header("Location:signin.php");
+    session_destroy();
+    
+
+   
+    
+?>
