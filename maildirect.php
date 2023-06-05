3bc8e886c1be213e8270f1cc793df137ac22d92b
sammpudi <111079306+sammpudi@users.noreply.github.com>
2023-06-05 18:48:45 +0200

    Create maildirect.php

---
+++ maildirect.php
@@ -0,0 +1,46 @@
+?php  include 'footer.php'     ?>
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank you for contacting us</title>
+    <link rel="icon" href="images/Creative-PhotoRoom.png-PhotoRoom.png" type="image/png">
+</head>
+<body> 
+    <h1 >Your input has been succsesfully submitted!</h1>
+      
+    <h2>Thank you for reaching out to us. We will be in touch with you soon</h2>
+
+    <style>
+        *{
+            background:#0F2D49;
+        }
+        h1{
+            color:yellow;
+            position: absolute;
+            top: 20%;
+            left: 20%;
+            font-size:25px;
+        }
+        h2{
+            color:yellow;
+            position: absolute;
+            top: 40%;
+            left: 20%;
+
+        }
+    </style>
+</body>
+</html>
