001f2aa52eaee1877f797893b068785f4ef0def0
sammpudi <111079306+sammpudi@users.noreply.github.com>
2023-06-05 18:57:14 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/facebook.css' rel='stylesheet'>
+    <link href='https://unpkg.com/css.gg@2.0.0/icons/css/twitter.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>ThriveMentor</title>
+</head>
+<body>
+
+   
+<div class="fwrap">
+  
+     <footer class="footer-distributed">
+
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="footer-left">
+    <h3>Thrive <span>Mentor.io</span></h3>
+    <p class="footer-icons">
+
+
+<a href="https://www.linkedin.com/company/thrivementor/?viewAsMember=true"><i class="fa fa-linkedin"></i></a>    
+
+<a href="https://twitter.com/ThriveMentor" class="fa fa-twitter"></a>
+<a href="https://www.facebook.com/profile.php?id=100093026431060"><i class="fa fa-facebook"></i></a>
+
+
+
+     
+     
+
+</p>
+    <p class="footer-links">
+        <a href="Home.php">Home</a>
+        |
+        <a href="About.php">About</a>
+        |
+        <a href="Services.php">Services</a>
+        |
+        <a href="Coaches.php">Coaches</a>
+        |
+        <a href="profile.php">Profile</a>
+    </p>
+   
+    
+</div>
+<div class="footer-center" style="background-color: #292c2f;">
+    <div class="bunting-road" style="background-color: #292c2f;">
+        <i class="fa-solid fa-location-dot" style="background-color: #292c2f;"></i>
+        <p style="background-color: #292c2f;"><span>UJ Bunting Road</span>Auckland  Park</p>
+    </div>
+    <div class="phone" style="background-color: #292c2f;">
+        <i class="fa fa-phone"></i>
+        <p>011 559 4555</p>
+    </div>
+    <div class="UJ" style="background-color: #292c2f;">
+        
+       
+    <i class="fa fa-envelope" style="font-size:36px"></i>
+        <p> mentorthrive79@gmail.com</p>
+    </div>
+   
+
+</div>
+<div class="footer-right">
+    <p class="footer-company-about">
+        <span>About the company</span>
+        <b>ThriveMentor.io</b> is a website that is built for people in need of help and willing to be assisted dealing with their issues <br>
+
+
+
+
+
+
+        
+    <strong class="copy" style="margin-top:20px;">&copy; 2023 Thrive Mentor. All rights reserved. <a href="terms-and-conditions.html">Terms of Service</a>
+    </strong>
+
+
+
+    </p>
+   
+
+
+
+
+</div>
+
+
+
+
+
+</footer>
+</div>
+
+<style>
+
+
+
+
+
+
+
+
+
+.fwrap {
+  position: relative;
+  z-index: 2;
+  
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 25%;
+  margin-top: 300px;
+  background-color: #292c2f;
+  z-index: 3;
+}
+
+.forrm{
+     margin-bottom:400px;
+}
+.footer-distributed{
+    background-color: #292c2f;
+    box-sizing: border-box;
+    widows: 100%;
+    text-align: left;
+    font: bold 10px sans-serif;
+    padding: 5px 5px 5px 5px;
+    margin-top:300px;
+}
+.footer-distributed .footer-left, .footer-distributed .footer-center, .footer-distributed .footer-right{
+    display: inline-block;
+    vertical-align: top;
+}
+.footer-distributed .footer-left{
+    width: 30%;
+    background-color: #292c2f;
+}
+.footer-distributed h3{
+    color: #ffff;
+    font: normal 36px 'cookie',cursive;
+    margin: 0;
+    background: #292c2f;
+}
+.footer-distributed h3 span{
+    color: #ccbf90;
+    font: normal 36px 'cookie',cursive;
+    background:#292c2f ;
+
+}
+.footer-distributed .footer-links{
+    color: #ffff;
+    margin: 20px 0 12px;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-links a{
+    display: inline-block;
+    line-height: 1.8;
+    text-decoration: none;
+    color: inherit;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-company-name{
+    color: #8f9296;
+    font-size: 14px;
+    font-weight: normal;
+    margin: 0;
+    background: #292c2f;
+}
+.footer-distributed .footer-center{
+    width: 35%;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-center i{
+    background-color: #33383b;
+    color: #ffff;
+    font-size: 25px;
+    width: 38px;
+    height: 38px;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 42px;
+    margin: 10px 15px;
+    vertical-align: middle;
+}
+.footer-distributed .footer-center i .fa-envelop{
+    font-size: 10px;
+    line-height: 38px;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-center p{
+    display: inline-block;
+    color: #ffff;
+    vertical-align: middle;
+    margin: 0;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-center p span{
+    display:block;
+    font-weight: normal;
+    font-size:14px;
+    line-height:2;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-center p a{
+    color:  #5383d3;
+    text-decoration: none;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-right{
+    width: 30%;
+}
+.footer-distributed .footer-company-about{
+    line-height: 20px;
+    color: #ffff;
+    font-size: 13px;
+    font-weight: normal;
+    margin: 0;
+    background:#292c2f;
+}
+.footer-distributed .footer-company-about span{
+    display: block;
+    color:  #5383d3;
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-icons{
+    margin-top: 25px;
+    display: block;
+    background-color: #292c2f;
+}
+.footer-distributed .footer-icons a{
+    display: inline-block;
+    width: 35px;
+    height: 35px;
+    cursor: pointer;
+    background-color:  #33383b;
+    border-radius: 2px;
+    font-size: 20px;
+    color: #ffffff;
+    text-align: center;
+    line-height: 35px;
+    margin-right: 3px;
+    margin-bottom: 5px;
+}
+.footer-distributed .footer-icons a:hover{
+    background-color: #3f71ea;
+}
+.footer-links a:hover{
+    color: #3f71ea;
+}
+
+
+
+
+
+
+
+
+</style>
+
+
+
+
+
+
+
+</body>
+</html>
